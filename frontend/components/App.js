c1cdd152453109c1b28f1a919f3d2fa67fcc9de8
Ben Frame <brewerytattooer@gmail.com>
2023-10-24 12:47:07 -0400

    Sprint Complete

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import axios from 'axios'
 import Character from './Character'
 
@@ -7,12 +7,43 @@ const urlPeople = 'http://localhost:9009/api/people'
 
 function App() {
   // ❗ Create state to hold the data from the API
+  const [characters, setCharacters] = useState([])
+  const [selectedCharacter, setSelectedCharacter] = useState(null)
   // ❗ Create effects to fetch the data and put it in state
+  useEffect(() => {
+    // axios.get(`${urlPeople}`)
+    //   .then(res => {
+    //     setCharacters(res.data)
+    //   })
+    const getStuff = async () => {
+      try {
+        const charRes = await axios.get(`${urlPeople}`);
+        const planRes = await axios.get(`${urlPlanets}`);
+
+        charRes.data.forEach((char) => {
+          const newHome = planRes.data.find(planet => planet.id === char.homeworld)
+          char.planetInfo = newHome
+        })
+        setCharacters(charRes.data)
+      }
+      catch (err) {
+        console.log(err);
+      }
+    }
+    getStuff();
+  },[])
+  
   return (
     <div>
       <h2>Star Wars Characters</h2>
       <p>See the README of the project for instructions on completing this challenge</p>
-      {/* ❗ Map over the data in state, rendering a Character at each iteration */}
+      {/* ❗ Map over the data in state, rendering a Character at each iteration */
+      characters.map((char) => {
+        return <Character key={char.id} info={char} 
+
+          />
+      })
+      }
     </div>
   )
 }
--- frontend/components/Character.js
+++ frontend/components/Character.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
 
-function Character() { // ❗ Add the props
+const StyledCharacter = styled.div``
+
+function Character({info}) { // ❗ Add the props
   // ❗ Create a state to hold whether the homeworld is rendering or not
+  const[homeworld, setHomeworld] = useState(false)
   // ❗ Create a "toggle" click handler to show or remove the homeworld
+  const toggleHomeWorld = (()=> setHomeworld(!homeworld))
+
   return (
-    <div>
-      {/* Use the same markup with the same attributes as in the mock */}
-    </div>
+    <StyledCharacter className= 'character-card' onClick={toggleHomeWorld}>
+      {/* Use the same markup with the same attributes as in the mock */
+      <>
+      <h3 className = 'character-name'>{info.name}</h3>
+      { homeworld && <p>Planet: <span className = "character-planet">{info.planetInfo.name}</span> </p> }
+      </>
+      }
+    </StyledCharacter>
   )
 }
 
